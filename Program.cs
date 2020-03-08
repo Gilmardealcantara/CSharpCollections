641fef1861539363a3d511b27f83710d25869873
Gilmar <gilmar.alcantara@ibm.com>
2020-03-08 00:01:08 -0300

    Array Collection

--- Program.cs
+++ Program.cs
@@ -12,13 +12,39 @@ namespace Collections
         static void Main(string[] args)
         {
             /* Collection: */
-            DynamicArray();
+            // DynamicArray();
             NonGenericCollection();
         }
 
         private static void NonGenericCollection()
         {
-            throw new NotImplementedException();
+            /*
+                System.Collection: 
+                Stack, Queue, LinkedList, SortedList, ArrayList, Hashtable
+                ArrayList: 
+                - Array vs ArrayList
+                    - Fix Lenght | VariableList
+                    - Not possible insert itens | We can insert itens int the middle
+                    - Not possible delete itens | we can delete itens from the middle
+                - Capacite increase when add itens 4 in 4 
+                
+            */
+            ArrayList al = new ArrayList(1);
+            Console.WriteLine(al.Capacity);
+            al.Add("1");
+            Console.WriteLine(al.Capacity);
+            al.Add(2); al.Add(3); al.Add(4);
+            Console.WriteLine(al.Capacity);
+            al.Add(5);
+            Console.WriteLine(al.Capacity);
+            al.Add(6); al.Add("777"); al.Add(8.9); al.Add(9);
+            Console.WriteLine(al.Capacity);
+            PrintCollection(al);
+            al.Insert(3, 0);
+            PrintCollection(al);
+            al.Remove("777");
+            al.RemoveAt(3);
+            PrintCollection(al);
         }
 
         static void ShowMemoryAddr(object obj)
@@ -26,6 +52,14 @@ namespace Collections
             GCHandle gch = GCHandle.Alloc(obj, GCHandleType.Pinned);
             IntPtr pObj = gch.AddrOfPinnedObject();
             Console.WriteLine(pObj.ToString());
+        }
+
+        static void PrintCollection(ICollection collection)
+        {
+            foreach (var item in collection)
+                Console.Write(item + ",");
+            Console.WriteLine();
+
         }
 
         static void DynamicArray()
@@ -39,8 +73,8 @@ namespace Collections
             */
             var arr = new char[4] { '1', '2', '3', '4' };
             ShowMemoryAddr(arr);
-            Array.Resize(ref arr, 5); // create a new array
-            Console.WriteLine(string.Join(',', arr));
+            Array.Resize(ref arr, 10); // create a new array
+            PrintCollection(arr);
             ShowMemoryAddr(arr);
         }
     }
